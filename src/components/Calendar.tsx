37ef92f341667f2b11f66584bc9dc61a07050b02
Dawson Hamera <77896436+CoderWarrior48@users.noreply.github.com>
2025-02-04 21:31:22 -0800

    new changes to calendar (may be bug with upredictable routing now)

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -11,6 +11,11 @@ import QRCode from "react-qr-code";
 interface CalendarProps {
     qrcode?: boolean;
     canEdit?: boolean;
+    events: any[];  // List of events from parent
+    attendance: any[]
+    onAddEvent: (newEvent: any) => void;  // Parent function to add event
+    onRemoveEvent: (eventId: string) => void;  // Parent function to remove event
+    loading: boolean;  // Loading state from parent
 }
 
 interface Event {
@@ -24,10 +29,7 @@ interface Event {
     verification_code: string;
 }
 
-const Calendar: React.FC<CalendarProps> = ({ canEdit }) => {
-    const [loading, setLoading] = useState(true);
-    const [attendance, setAttendance] = useState<string[]>([]);
-    const [events, setEvents] = useState<Event[]>([]);
+const Calendar: React.FC<CalendarProps> = ({ events, attendance, onAddEvent, onRemoveEvent, loading, qrcode, canEdit }) => {
     const [isOpen, setIsOpen] = useState(false);
     const [isStartTimeOpen, setIsStartTimeOpen] = useState(false);
     const [isEndTimeOpen, setIsEndTimeOpen] = useState(false);
@@ -51,181 +53,155 @@ const Calendar: React.FC<CalendarProps> = ({ canEdit }) => {
         setVerificationCode(uuidv4().substring(0, 8))
     }
 
-    const addEvent = async () => {
-        console.log("Adding event...")
+    const handleAddEvent = () => {
+        // Call the parent’s onAddEvent function to add the event
+        onAddEvent({ event_name: eventName, event_type: eventType, event_details: eventDetails, event_location: eventLocation, scheduled_by: `${userState?.firstName} ${userState?.lastName}`, verification_code: verificationCode, event_date: eventDate, start_time: startTime, end_time: endTime });
+        setIsOpen(false);  // Close modal after adding
+    };
 
-        try {
-            const result = await axios.post('https://dawson.hamera.com/api/add_event.php', { event_name: eventName, event_type: eventType, event_details: eventDetails, event_location: eventLocation, scheduled_by: `${userState?.firstName} ${userState?.lastName}`, verification_code: verificationCode, event_date: eventDate, start_time: startTime, end_time: endTime });
-            console.log(result.data)
-            if (result.data.error) {
-                console.log(result.data.error)
-                setIsInvalid(true)
-                setErrorMessage(result.data.error)
-            } else {
-                setIsInvalid(false)
-                setIsOpen(false)
-                fetchData()
-            }
-        } catch (error) {
-            console.error("Error", error);
+    const handleRemoveEvent = (eventId: string) => {
+        console.log("removing 1...")
+        // Call the parent’s onRemoveEvent function to remove the event
+        onRemoveEvent(eventId);
+    };
 
-        }
-    }
+    const attending = events.filter(event => attendance.includes(event.event_id));
+    const other = events.filter(event => !attendance.includes(event.event_id));
 
 
-    const fetchData = async () => {
-        const attendance = await axios.post("https://dawson.hamera.com/api/get_attendance.php", { user_id: userState?.user_id });
-        const events = await axios.get("https://dawson.hamera.com/api/get_events.php");
-        const attendedEvents = attendance.data.map((event: any) => event.event_id)
-        setAttendance(attendedEvents)
-        setEvents(events.data)
-        setLoading(false)
-    }
-    const removeEvent = async (code: String) => {
-        try {
-            const result = await axios.post('https://dawson.hamera.com/api/remove_event.php', { verification_code: code });
-            fetchData()
-            console.log("Deleted successfully", result)
-        } catch (error) {
-            console.error(error);
+    const renderEvents = (events: Event[], color: string) => (
+        <>
+            {events.map((event: Event) => (
+                <IonItemSliding key={event.event_id}>
+                    <IonItem >
+                        <IonCard style={{borderLeft: `6px solid ${color}` /*backgroundColor: "var(--ion-color-light-shade)"*/}}>
+                            <IonGrid>
+                                <IonRow>
+                                    <IonCol size="1" className="vertical-container">
+                                        <p className="vertical">{event.event_type}</p>
+                                    </IonCol>
+                                    <IonCol>
+                                        <IonCardHeader>
+                                            <IonCardTitle>{event.event_name}</IonCardTitle>
+                                            <IonCardSubtitle>
+                                                {new Date(`1970-01-01T${event.start_time}Z`).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }) + " - "}
+                                                {new Date(`1970-01-01T${event.end_time}Z`).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}
+                                            </IonCardSubtitle>
+                                        </IonCardHeader>
 
-        }
-    }
+                                        <IonCardContent>
+                                            {event.event_details}
+                                        </IonCardContent>
+                                    </IonCol>
 
-    useEffect(
-        React.useCallback(() => {
-            setLoading(true)
-            fetchData();
-        }, [])
-        , []);
+                                    <IonCol size="4">
+                                        <div style={{ justifyItems: 'center' }}>
+                                            <h4>{new Date(event.event_date).toLocaleString('en-US', { month: 'short', day: 'numeric' }).toUpperCase()}</h4>
+                                            {qrcode ? (
+                                                <QRCode
+                                                    value={event.verification_code}
+                                                    size={100}
+                                                    bgColor="transparent"
+                                                    fgColor="var(--ion-color-primary)"
+                                                />
+                                            ) : (
+                                                <IonIcon icon={checkmarkCircle} size="large"></IonIcon>
+                                            )}
+                                        </div>
+                                    </IonCol>
+                                </IonRow>
+                            </IonGrid>
 
-    const renderEvents = events.map((event: Event) => (
-        <IonItemSliding>
-            <IonItem>
-                <IonCard className="card" key={event.event_id}>
-                    <IonGrid>
-                        <IonRow>
-                            <IonCol size="1" className="vertical-container">
-                                <p className="vertical">{event.event_type}</p>
-                            </IonCol>
-                            <IonCol>
-                                <IonCardHeader>
-                                    <IonCardTitle>{event.event_name}</IonCardTitle>
-                                    <IonCardSubtitle>
-                                        {new Date(`1970-01-01T${event.start_time}Z`).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }) + " - "}
-                                        {new Date(`1970-01-01T${event.end_time}Z`).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}
-                                    </IonCardSubtitle>
-                                </IonCardHeader>
+                        </IonCard>
+                    </IonItem>
 
-                                <IonCardContent>
-                                    {event.event_details}
-                                </IonCardContent>
-                            </IonCol>
-
-                            <IonCol size="4">
-                                <div style={{ justifyItems: 'center' }}>
-                                    <h4>{new Date(event.event_date).toLocaleString('en-US', { month: 'short', day: 'numeric' }).toUpperCase()}</h4>
-                                    {userState?.role === 'admin' ? (
-                                        <QRCode
-                                            value={event.verification_code}
-                                            size={100}
-                                            bgColor="transparent"
-                                            fgColor="var(--ion-color-primary)"
-                                        />
-                                    ) : (
-                                        <IonIcon icon={checkmarkCircle} size="large"></IonIcon>
-                                    )}
-                                </div>
-                            </IonCol>
-                        </IonRow>
-                    </IonGrid>
-
-                </IonCard>
-            </IonItem>
-
-            <IonItemOptions side="end">
-                <IonItemOption color="danger">
-                    <IonIcon slot="icon-only" icon={trash}></IonIcon>
-                </IonItemOption>
-            </IonItemOptions>
-        </IonItemSliding>
-    ))
+                    <IonItemOptions side="end">
+                        <IonItemOption color="danger" onClick={() => handleRemoveEvent(event.event_id)}>
+                            <IonIcon slot="icon-only" icon={trash}></IonIcon>
+                        </IonItemOption>
+                    </IonItemOptions>
+                </IonItemSliding>
+            ))}
+            </>
+    )
 
     const editEvents = (
-        <IonPage>
-            <IonContent>
-                <IonButton onClick={() => setIsOpen(true)} className="center">Add</IonButton>
-                <IonModal isOpen={isOpen}>
-                    <IonHeader>
-                        <IonToolbar>
-                            <IonTitle>Modal</IonTitle>
-                            <IonButtons slot="end">
-                                <IonButton onClick={() => setIsOpen(false)}>Close</IonButton>
-                            </IonButtons>
-                        </IonToolbar>
-                    </IonHeader>
-                    <IonContent className="ion-padding">
-                        <IonList>
-                            <IonItem>
-                                <IonInput label="Name" value={eventName} onIonInput={(event) => setEventName((event.detail.value ?? '') as string)}></IonInput>
-                            </IonItem>
-                            <IonItem>
-                                <IonInput label="Type" value={eventType} onIonInput={(event) => setEventType((event.detail.value ?? '') as string)}></IonInput>
-                            </IonItem>
-                            <IonItem>
-                                <IonInput label="Details" value={eventDetails} onIonInput={(event) => setEventDetails((event.detail.value ?? '') as string)}></IonInput>
-                            </IonItem>
-                            <IonItem>
-                                <IonInput label="Location" value={eventLocation} onIonInput={(event) => setEventLocation((event.detail.value ?? '') as string)}></IonInput>
-                            </IonItem>
-                            <IonItem>
-                                <IonButton onClick={() => setIsStartTimeOpen(true)}>Start time</IonButton> <p style={{ marginLeft: 15 }}>{new Date(startTime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
-                            </IonItem>
-                            <IonItem>
-                                <IonButton onClick={() => setIsEndTimeOpen(true)}>End time</IonButton> <p style={{ marginLeft: 15 }}>{new Date(endTime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
-                            </IonItem>
-                            <IonItem>
-                                <IonButton onClick={() => setIsDateOpen(true)}>Date</IonButton> <p style={{ marginLeft: 15 }}>{new Date(eventDate).toLocaleString('en-US', { month: 'short', day: 'numeric' })}</p>
-                            </IonItem>
-                            <IonItem>
-                                <IonInput label="Code" value={verificationCode} onIonInput={(event) => setVerificationCode((event.detail.value ?? '') as string)}></IonInput>
-                                <IonButton onClick={() => generateCode()}>Generate</IonButton>
-                            </IonItem>
-                        </IonList>
-                        <IonButton className="center" onClick={() => addEvent()}>Add</IonButton>
-                        <p>{errorMessage}</p>
-                    </IonContent>
-                    <IonModal isOpen={isStartTimeOpen} initialBreakpoint={0.5} breakpoints={[0.5]} onDidDismiss={() => setIsStartTimeOpen(false)}>
-                        <div className="center">
-                            <IonDatetime presentation="time" className="timedate" value={startTime} onIonChange={(event) => { setStartTime((event.detail.value ?? '') as string) }}></IonDatetime>
-                        </div>
-                    </IonModal>
-                    <IonModal isOpen={isEndTimeOpen} initialBreakpoint={0.5} breakpoints={[0.5]} onDidDismiss={() => setIsEndTimeOpen(false)}>
-                        <div className="center">
-                            <IonDatetime presentation="time" className="timedate" value={endTime} onIonChange={(event) => { setEndTime((event.detail.value ?? '') as string) }}></IonDatetime>
-                        </div>
-                    </IonModal>
-                    <IonModal isOpen={isDateOpen} initialBreakpoint={0.5} breakpoints={[0.5]} onDidDismiss={() => setIsDateOpen(false)}>
-                        <div className="center">
-                            <IonDatetime presentation="date" className="timedate" value={eventDate} onIonChange={(event) => { setEventDate((event.detail.value ?? '') as string) }}></IonDatetime>
-                        </div>
-                    </IonModal>
+        <>
+            <IonButton onClick={() => setIsOpen(true)} className="center">Add</IonButton>
+            <IonModal isOpen={isOpen}>
+                <IonHeader>
+                    <IonToolbar>
+                        <IonTitle>Modal</IonTitle>
+                        <IonButtons slot="end">
+                            <IonButton onClick={() => setIsOpen(false)}>Close</IonButton>
+                        </IonButtons>
+                    </IonToolbar>
+                </IonHeader>
+                <IonContent className="ion-padding">
+                    <IonList>
+                        <IonItem>
+                            <IonInput label="Name" value={eventName} onIonInput={(event) => setEventName((event.detail.value ?? '') as string)}></IonInput>
+                        </IonItem>
+                        <IonItem>
+                            <IonInput label="Type" value={eventType} onIonInput={(event) => setEventType((event.detail.value ?? '') as string)}></IonInput>
+                        </IonItem>
+                        <IonItem>
+                            <IonInput label="Details" value={eventDetails} onIonInput={(event) => setEventDetails((event.detail.value ?? '') as string)}></IonInput>
+                        </IonItem>
+                        <IonItem>
+                            <IonInput label="Location" value={eventLocation} onIonInput={(event) => setEventLocation((event.detail.value ?? '') as string)}></IonInput>
+                        </IonItem>
+                        <IonItem>
+                            <IonButton onClick={() => setIsStartTimeOpen(true)}>Start time</IonButton> <p style={{ marginLeft: 15 }}>{new Date(startTime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
+                        </IonItem>
+                        <IonItem>
+                            <IonButton onClick={() => setIsEndTimeOpen(true)}>End time</IonButton> <p style={{ marginLeft: 15 }}>{new Date(endTime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
+                        </IonItem>
+                        <IonItem>
+                            <IonButton onClick={() => setIsDateOpen(true)}>Date</IonButton> <p style={{ marginLeft: 15 }}>{new Date(eventDate).toLocaleString('en-US', { month: 'short', day: 'numeric' })}</p>
+                        </IonItem>
+                        <IonItem>
+                            <IonInput label="Code" value={verificationCode} onIonInput={(event) => setVerificationCode((event.detail.value ?? '') as string)}></IonInput>
+                            <IonButton onClick={() => generateCode()}>Generate</IonButton>
+                        </IonItem>
+                    </IonList>
+                    <IonButton className="center" onClick={() => handleAddEvent()}>Add</IonButton>
+                    <p>{errorMessage}</p>
+                </IonContent>
+                <IonModal isOpen={isStartTimeOpen} initialBreakpoint={0.5} breakpoints={[0.5]} onDidDismiss={() => setIsStartTimeOpen(false)}>
+                    <div className="center">
+                        <IonDatetime presentation="time" className="timedate" value={startTime} onIonChange={(event) => { setStartTime((event.detail.value ?? '') as string) }}></IonDatetime>
+                    </div>
                 </IonModal>
-                {renderEvents}
-            </IonContent>
-        </IonPage>
+                <IonModal isOpen={isEndTimeOpen} initialBreakpoint={0.5} breakpoints={[0.5]} onDidDismiss={() => setIsEndTimeOpen(false)}>
+                    <div className="center">
+                        <IonDatetime presentation="time" className="timedate" value={endTime} onIonChange={(event) => { setEndTime((event.detail.value ?? '') as string) }}></IonDatetime>
+                    </div>
+                </IonModal>
+                <IonModal isOpen={isDateOpen} initialBreakpoint={0.5} breakpoints={[0.5]} onDidDismiss={() => setIsDateOpen(false)}>
+                    <div className="center">
+                        <IonDatetime presentation="date" className="timedate" value={eventDate} onIonChange={(event) => { setEventDate((event.detail.value ?? '') as string) }}></IonDatetime>
+                    </div>
+                </IonModal>
+            </IonModal>
+        </>
     )
 
     if (loading) {
         return <IonProgressBar type="indeterminate"></IonProgressBar>;
     }
+    return (
+        <div>
+            {canEdit && editEvents}
+            <IonList lines="none">
+            <h3 className="center">Attending</h3>
+            {renderEvents(attending, "var(--ion-color-success)")}
+            <h3 className="center">Other</h3>
+            {renderEvents(other, "var(--ion-color-primary)")}
+            </IonList>
+        </div>
+    )
 
-
-    if (canEdit) {
-        return editEvents
-    } else {
-        return <div>{renderEvents}</div>
-    }
 
 }
 
--- src/pages/app/CalendarPage.tsx
+++ src/pages/app/CalendarPage.tsx
@@ -1,20 +1,82 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Calendar from '../../components/Calendar';
+import { useAuth } from '../../providers/AuthProvider';
+import { IonContent, IonRefresher, IonRefresherContent, RefresherEventDetail } from '@ionic/react';
 
-import React, { useState } from "react"
-import Calendar from "../../components/Calendar"
-import './CalendarPage.css';
-import { IonPage } from "@ionic/react";
-import { refresh } from "ionicons/icons";
-import { useAuth } from "../../providers/AuthProvider";
+const ParentComponent: React.FC = () => {
+    const [attendance, setAttendance] = useState([]);
+    const [events, setEvents] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-
-const CalendarPage: React.FC = () => {
     const { userState } = useAuth()
 
-    return (
-        <IonPage>
-            <Calendar canEdit={userState?.role == 'admin'}/>
-        </IonPage>
-    )
-}
+    // Fetch events when the component mounts
+    useEffect(() => {
+        fetchEvents();
+    }, []);
 
-export default CalendarPage;
+    const fetchEvents = async () => {
+        console.log('events fetched')
+        setLoading(true);
+        try {
+            const events = await axios.get('https://dawson.hamera.com/api/get_events.php');
+            const attendance = await axios.post("https://dawson.hamera.com/api/get_attendance.php", { user_id: userState?.user_id });
+            setEvents(events.data);
+            setAttendance(attendance.data)
+        } catch (error) {
+            console.error("Error fetching events:", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const handleAddEvent = async (newEvent: any) => {
+        try {
+            // Send the new event data to your backend
+            await axios.post('https://dawson.hamera.com/api/add_event.php', newEvent);
+
+            // After adding, refresh the events
+            fetchEvents();
+        } catch (error) {
+            console.error("Error adding event:", error);
+        }
+    };
+
+    const handleRemoveEvent = async (eventId: string) => {
+        console.log("removing 2...")
+        try {
+            // Send the request to delete the event
+            await axios.post('https://dawson.hamera.com/api/remove_event.php', { event_id: eventId });
+
+            // After deleting, refresh the events
+            fetchEvents();
+        } catch (error) {
+            console.error("Error removing event:", error);
+        }
+    };
+
+    async function handleRefresh(event: CustomEvent<RefresherEventDetail>) {
+        await fetchEvents()
+          event.detail.complete();
+      }
+
+    return (
+        <IonContent>
+            <IonRefresher slot="fixed" onIonRefresh={handleRefresh}>
+                <IonRefresherContent></IonRefresherContent>
+            </IonRefresher>
+            <Calendar
+                attendance={attendance}
+                events={events}
+                onAddEvent={handleAddEvent}
+                onRemoveEvent={handleRemoveEvent}
+                loading={loading}
+                canEdit={userState?.role === 'admin'}
+                qrcode={userState?.role === 'admin'}
+            />
+        </IonContent>
+    );
+};
+
+export default ParentComponent;
